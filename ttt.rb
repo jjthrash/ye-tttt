577590b25d7c2278b279f7c4bc86b5a8defd64ce
Jimmy Thrasher <jimmy@jimmythrasher.com>
2009-09-04 22:26:34 -0400

    Move towards being able to show Xs and Os

--- ttt.rb
+++ ttt.rb
@@ -1,11 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'json'
 
 class Board
-    def initialize(w, h)
+    def initialize(w, h, cells = nil)
         @stride = w
-        @cells = Array.new(w*h)
+        @cells = cells || Array.new(w*h)
+    end
+
+    def to_s
+        [ @stride, @cells ].to_json
+    end
+
+    def self.from_s(s)
+        stride, cells = JSON.parse(s)
+        result = Board.new(stride, cells.length / stride, cells)
     end
 
     def cell_type(index)
@@ -32,16 +42,34 @@ class Board
             yield cell, cell_type(index), "cell#{index}"
         end
     end
+
+    def play(action)
+    end
 end
+
+module Helpers
+    def image_for_cell(cell)
+        if ['x','o'].include? cell
+            haml "%img{src => '/#{cell}.png'}"
+        end
+    end
+end
+
+helpers(Helpers)
 
 get '/' do
     haml 'test'
 end
 
-get '/play' do
-    haml :board, :locals => { :board => Board.new(4,4) }
-end
-
-get '/play/(.*)' do |layout|
-    haml :board
+get '/play/?:action' do |action|
+    cookie = request.cookies['board']
+    board =
+        if cookie
+            Board.from_s(cookie)
+        else
+            Board.new(4,4)
+        end
+    board.play action
+    set_cookie 'board', board.to_s
+    haml :board, :locals => { :board => board }
 end
