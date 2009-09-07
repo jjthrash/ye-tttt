ddd57f4ef24279b787a5fa73bfd8a2340f1e1fbe
Jimmy Thrasher <jimmy@jimmythrasher.com>
2009-09-07 15:27:55 -0400

    Add clicky clicky x

--- ttt.rb
+++ ttt.rb
@@ -6,7 +6,7 @@ require 'json'
 class Board
     def initialize(w, h, cells = nil)
         @stride = w
-        @cells = cells || Array.new(w*h)
+        @cells = cells || Array.new(w*h, '-')
     end
 
     def to_s
@@ -37,20 +37,36 @@ class Board
         end
     end
 
+    def [](index)
+        @cells[index]
+    end
+
     def each
         @cells.each_with_index do |cell, index|
             yield cell, cell_type(index), "cell#{index}"
         end
     end
 
-    def play(action)
+    def play(marker, index)
+        raise "There is already an #{@cells[index]} at index #{index}" if @cells[index] != '-'
+
+        @cells[index] = marker
     end
 end
 
 module Helpers
     def image_for_cell(cell)
         if ['x','o'].include? cell
-            haml "%img{src => '/#{cell}.png'}"
+            haml "%img{:src => '/#{cell}.png'}"
+        end
+    end
+
+    def get_board(request)
+        cookie = request.cookies['board']
+        if cookie
+            Board.from_s(cookie)
+        else
+            Board.new(4,4)
         end
     end
 end
@@ -58,18 +74,14 @@ end
 helpers(Helpers)
 
 get '/' do
-    haml 'test'
+    board = Board.new(4, 4)
+    response.set_cookie 'board', board.to_s
+    haml :board, :locals => { :board => board }
 end
 
-get '/play/?:action' do |action|
-    cookie = request.cookies['board']
-    board =
-        if cookie
-            Board.from_s(cookie)
-        else
-            Board.new(4,4)
-        end
-    board.play action
-    set_cookie 'board', board.to_s
-    haml :board, :locals => { :board => board }
+post '/play' do
+    board = get_board(request)
+    board.play params[:marker], params[:index].to_i
+    response.set_cookie 'board', board.to_s
+    image_for_cell(params[:marker])
 end
